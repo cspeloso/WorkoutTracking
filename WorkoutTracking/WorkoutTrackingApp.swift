250b769fa3041146f32c45e9f939d347cb6666cd
cspeloso <chrispeloso2@gmail.com>
2022-02-24 23:00:38 -0500

    Initial Commit

---
+++ WorkoutTracking/WorkoutTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackingApp.swift
+//  WorkoutTracking
+//
+//  Created by Chris Peloso on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
